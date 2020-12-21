d923f6648059af06f7a5d155203cdc6705ed56f2
whitewalker608 <nitingoel608@gmail.com>
2020-12-21 13:26:27 +0530

    adding replace in beacon

--- beacon/create.go
+++ beacon/create.go
@@ -55,68 +55,77 @@ func CreateCommand() *cli.Command {
 			},
 		},
 		Action: func(c *cli.Context) error {
-			out, _ := exec.Command("sudo", "supervisorctl", "status", program).Output()
-			if !strings.Contains(string(out), "no such process") {
-				// Already exists
-				return errors.New("Already exists")
-			}
-
-			// User details
-			usr, err := util.GetUser()
-			if err != nil {
-				return err
-			}
-
-			if version == "latest" {
-				fmt.Println("beacon fetching latest binaries...")
-				latestVersion, err := util.FetchLatestVersion("beacon")
-				if err != nil {
-					return err
-				}
-				version = latestVersion
-				fmt.Println("beacon latest binary version: ", latestVersion)
-			}
-			// Beacon executable
-			err = util.Fetch("https://storage.googleapis.com/marlin-artifacts/bin/beacon-"+version+"-"+runtime.GOOS+"-"+runtime.GOARCH, usr.HomeDir+"/.marlin/ctl/bin/beacon-"+version, usr.Username, true, false)
-			if err != nil {
-				return err
-			}
-
-			// Beacon config
-			err = util.Fetch("https://storage.googleapis.com/marlin-artifacts/configs/beacon-"+version+".conf", usr.HomeDir+"/.marlin/ctl/configs/beacon-"+version+".conf", usr.Username, false, false)
-			if err != nil {
-				return err
-			}
-
-			err = util.TemplatePlace(
-				usr.HomeDir+"/.marlin/ctl/configs/beacon-"+version+".conf",
-				"/etc/supervisor/conf.d/"+program+".conf",
-				struct {
-					Program, User, UserHome                  string
-					DiscoveryAddr, HeartbeatAddr, BeaconAddr string
-					Version                                  string
-				}{
-					program, usr.Username, usr.HomeDir, discovery_addr, heartbeat_addr, beacon_addr, version,
-				},
-			)
-			if err != nil {
-				return err
-			}
-
-			_, err = exec.Command("supervisorctl", "reread").Output()
-			if err != nil {
-				return err
-			}
-
-			_, err = exec.Command("supervisorctl", "add", program).Output()
-			if err != nil {
-				return err
-			}
-
-			output, err := exec.Command("supervisorctl", "status").Output()
-			fmt.Print(string(output))
-
-			return nil
+			return Create(discovery_addr, heartbeat_addr, beacon_addr, program, version)
 		},
 	}
+}
+
+func Create(discovery_addr string,
+	heartbeat_addr string,
+	beacon_addr string,
+	program string,
+	version string) error {
+
+	out, _ := exec.Command("sudo", "supervisorctl", "status", program).Output()
+	if !strings.Contains(string(out), "no such process") {
+		// Already exists
+		return errors.New("Already exists")
+	}
+
+	// User details
+	usr, err := util.GetUser()
+	if err != nil {
+		return err
+	}
+
+	if version == "latest" {
+		fmt.Println("beacon fetching latest binaries...")
+		latestVersion, err := util.FetchLatestVersion("beacon")
+		if err != nil {
+			return err
+		}
+		version = latestVersion
+		fmt.Println("beacon latest binary version: ", latestVersion)
+	}
+	// Beacon executable
+	err = util.Fetch("https://storage.googleapis.com/marlin-artifacts/bin/beacon-"+version+"-"+runtime.GOOS+"-"+runtime.GOARCH, usr.HomeDir+"/.marlin/ctl/bin/beacon-"+version, usr.Username, true, false)
+	if err != nil {
+		return err
+	}
+
+	// Beacon config
+	err = util.Fetch("https://storage.googleapis.com/marlin-artifacts/configs/beacon-"+version+".conf", usr.HomeDir+"/.marlin/ctl/configs/beacon-"+version+".conf", usr.Username, false, false)
+	if err != nil {
+		return err
+	}
+
+	err = util.TemplatePlace(
+		usr.HomeDir+"/.marlin/ctl/configs/beacon-"+version+".conf",
+		"/etc/supervisor/conf.d/"+program+".conf",
+		struct {
+			Program, User, UserHome                  string
+			DiscoveryAddr, HeartbeatAddr, BeaconAddr string
+			Version                                  string
+		}{
+			program, usr.Username, usr.HomeDir, discovery_addr, heartbeat_addr, beacon_addr, version,
+		},
+	)
+	if err != nil {
+		return err
+	}
+
+	_, err = exec.Command("supervisorctl", "reread").Output()
+	if err != nil {
+		return err
+	}
+
+	_, err = exec.Command("supervisorctl", "add", program).Output()
+	if err != nil {
+		return err
+	}
+
+	output, err := exec.Command("supervisorctl", "status").Output()
+	fmt.Print(string(output))
+
+	return nil
 }
--- beacon/destroy.go
+++ beacon/destroy.go
@@ -24,34 +24,38 @@ func DestroyCommand() *cli.Command {
 			},
 		},
 		Action: func(c *cli.Context) error {
-			out, _ := exec.Command("sudo", "supervisorctl", "status", program).Output()
-			if strings.Contains(string(out), "no such process") {
-				return errors.New("Not found")
-			}
-
-			_, err := exec.Command("sudo", "supervisorctl", "stop", program).Output()
-			if err != nil {
-				return err
-			}
-
-			_, err = exec.Command("sudo", "supervisorctl", "remove", program).Output()
-			if err != nil {
-				return err
-			}
-
-			_, err = exec.Command("sudo", "rm", "/etc/supervisor/conf.d/"+program+".conf").Output()
-			if err != nil {
-				return err
-			}
-
-			_, err = exec.Command("supervisorctl", "reread").Output()
-			if err != nil {
-				return err
-			}
-
-			fmt.Println("beacon destroyed")
-
-			return nil
+			return Destroy(program)
 		},
 	}
+}
+
+func Destroy(program string) error {
+	out, _ := exec.Command("sudo", "supervisorctl", "status", program).Output()
+	if strings.Contains(string(out), "no such process") {
+		return errors.New("Not found")
+	}
+
+	_, err := exec.Command("sudo", "supervisorctl", "stop", program).Output()
+	if err != nil {
+		return err
+	}
+
+	_, err = exec.Command("sudo", "supervisorctl", "remove", program).Output()
+	if err != nil {
+		return err
+	}
+
+	_, err = exec.Command("sudo", "rm", "/etc/supervisor/conf.d/"+program+".conf").Output()
+	if err != nil {
+		return err
+	}
+
+	_, err = exec.Command("supervisorctl", "reread").Output()
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("beacon destroyed")
+
+	return nil
 }
